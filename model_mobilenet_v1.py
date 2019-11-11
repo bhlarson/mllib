2c7f97cd8f3696f973be3f38e86d9677237473c2
Brad Larson <bhlarson@gmail.com>
2019-11-11 12:45:19 -0800

    Merge mobilenet model

--- model_mobilenet_v1.py
+++ model_mobilenet_v1.py
@@ -1,33 +1,46 @@
 from mobilenet_v1 import make_mobilenet_v1
 from augmentations.py import augment
 
+def loss(logits, labels, num_classes):
+    #one_hots = tf.one_hot(labels, num_classes)
+    #weights = tf.reduce_sum(one_hots, axis=3)
+
+    max_x_ent_loss = tf.reduce_mean((tf.losses.sparse_softmax_cross_entropy(labels=labels,logits=logits)))
+    return max_x_ent_loss
+
+
 def mobilenet_v1_model_fn(features, labels, mode, params = {height = 512, width=512,data_format="NHWC"}):
 
-    images_uint8 = features["image"]
+    images = features["image"]
+    images = tf.image.convert_image_dtype(images, tf.float32)
+    images = tf.cast(
+        tf.map_fn(preprocessing.mean_image_addition, features),
+        tf.float32)
     
-    # Convert the range of the pixels from 0 - 255 to 0.0 - 1.0
-    image = tf.image.convert_image_dtype(images_uint8, tf.float32)
-
     if data_format == 'NHWC':
         # Convert the inputs from channels_last (NHWC) to channels_first (NCHW).
         # This provides a large performance boost on GPU. See
         # https://www.tensorflow.org/performance/performance_guide#data_formats
-        image = tf.transpose(image, [0, 3, 1, 2])
+        images = tf.transpose(images, [0, 2, 1, 3], name="nhwc_input")
 
     # Augment dataset when training
     if mode == tf.contrib.learn.ModeKeys.TRAIN:
-        image = augment(image)
+        images = augment(images, params)
 
-    image = tf.image.resize_with_crop_or_pad(image,params.height,params.width)
+    images = tf.image.per_image_standardization(images)
 
-    make_mobilenet_v1
-    logits = make_mobilenet_v1(image, mode)  
+    images = tf.image.resize_with_crop_or_pad(images,params.height,params.width)
 
-    #predictions = {
-    #    'class_ids': pred,
-        #'probabilities': tf.nn.softmax(logits),
-    #    'logits': logits,
-    #}
+    # make_mobilenet_v1
+    logits = make_mobilenet_v1(images, mode)  
+
+    predictions = {
+        # Returns the highest prediction from the output of logits.
+        tf.contrib.learn.PredictionKey.CLASSES : tf.argmax(logits, axis=1, name="predicted_classes"),
+        # Softmax squashes arbitrary real values into a range of 0 to 1 and returns the
+        # probabilities of each of the classes. Name is used for logging.
+        tf.contrib.learn.PredictionKey.PROBABILITIES : tf.nn.softmax(logits, name="predicted_probability")
+    }
 
     # Compute predictions.
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -47,7 +60,6 @@ def mobilenet_v1_model_fn(features, labels, mode, params = {height = 512, width=
     #accuracy = tf.metrics.accuracy(labels=labels,predictions=predictions,name='acc_op')
     #tf.summary.scalar('accuracy', accuracy[1])
     
-
     if mode == tf.estimator.ModeKeys.EVAL:
     # Compute evaluation metrics.
       #metrics = {'accuracy': accuracy}

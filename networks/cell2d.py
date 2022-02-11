da0a662668763cb6b601eea75d650c319a50a72d
Brad Larson <bhlarson@gmail.com>
2022-02-10 21:24:29 -0800

    crisp & multi-gpu training

--- networks/cell2d.py
+++ networks/cell2d.py
@@ -22,11 +22,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from mlflow import log_metric, log_param, log_artifacts
-#import adabound
 
 sys.path.insert(0, os.path.abspath(''))
 from utils.torch_util import count_parameters, model_stats, model_weights
-from utils.jsonutil import ReadDictJson, WriteDictJson
+from utils.jsonutil import ReadDictJson, WriteDictJson, str2bool
 from utils.s3 import s3store, Connect
 from torch.utils.tensorboard import SummaryWriter
 from networks.totalloss import TotalLoss
@@ -767,85 +766,6 @@ class Classify(nn.Module):
             
         return current_weights, self.total_trainable_weights, remnent
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if isinstance(v, int):
-        return not(v==0)
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Process arguments')
-
-    parser.add_argument('-d', '--debug', action='store_true',help='Wait for debuggee attach')   
-    parser.add_argument('-debug_port', type=int, default=3000, help='Debug port')
-    parser.add_argument('-debug_address', type=str, default='0.0.0.0', help='Debug port')
-    parser.add_argument('-fast', action='store_true', help='Fast run with a few iterations')
-    parser.add_argument('-description', type=str, default="", help='Training description to record with resuts')
-    parser.add_argument('-credentails', type=str, default='creds.json', help='Credentials file.')
-
-    parser.add_argument('-prune', type=str2bool, default=False)
-    parser.add_argument('-train', type=str2bool, default=True)
-    parser.add_argument('-infer', type=str2bool, default=True)
-    parser.add_argument('-search_structure', type=str2bool, default=True)
-    parser.add_argument('-onnx', type=str2bool, default=True)
-    parser.add_argument('-job', action='store_true',help='Run as job')
-
-    parser.add_argument('-resnet_len', type=int, choices=[18, 34, 50, 101, 152, 20, 32, 44, 56, 110], default=56, help='Run description')
-
-    parser.add_argument('-dataset_path', type=str, default='./dataset', help='Local dataset path')
-    parser.add_argument('-model', type=str, default='model')
-
-    parser.add_argument('-learning_rate', type=float, default=1e-1, help='Training learning rate')
-    parser.add_argument('-learning_rate_decay', type=float, default=0.1, help='Rate decay multiple')
-    parser.add_argument('-rate_schedule', type=json.loads, default='[60, 80, 100, 105, 110, 115]', help='Training learning rate')
-    #parser.add_argument('-rate_schedule', type=json.loads, default='[10, 15, 17, 19, 30, 32]', help='Training learning rate')
-    parser.add_argument('-momentum', type=float, default=0.9, help='Learning Momentum')
-    parser.add_argument('-weight_decay', type=float, default=0.0001)
-
-    parser.add_argument('-batch_size', type=int, default=400, help='Training batch size')
-    parser.add_argument('-epochs', type=int, default=20, help='Training epochs')
-    parser.add_argument('-model_type', type=str,  default='Classification')
-    parser.add_argument('-model_class', type=str,  default='CIFAR10')
-    parser.add_argument('-model_src', type=str,  default=None)
-    parser.add_argument('-model_dest', type=str, default="crisp20220209_t50_00")
-    parser.add_argument('-cuda', type=bool, default=True)
-    parser.add_argument('-k_structure', type=float, default=1e1, help='Structure minimization weighting factor')
-    parser.add_argument('-target_structure', type=float, default=0.5, help='Structure minimization weighting factor')
-    parser.add_argument('-batch_norm', type=bool, default=True)
-    parser.add_argument('-dropout_rate', type=float, default=0.0, help='Dropout probability gain')
-    parser.add_argument('-weight_gain', type=float, default=11.0, help='Convolution norm tanh weight gain')
-    parser.add_argument('-sigmoid_scale', type=float, default=5.0, help='Sigmoid scale domain for convolution channels weights')
-    parser.add_argument('-feature_threshold', type=float, default=0.5, help='tanh pruning threshold')
-
-    parser.add_argument('-augment_rotation', type=float, default=0.0, help='Input augmentation rotation degrees')
-    parser.add_argument('-augment_scale_min', type=float, default=1.00, help='Input augmentation scale')
-    parser.add_argument('-augment_scale_max', type=float, default=1.00, help='Input augmentation scale')
-    parser.add_argument('-augment_translate_x', type=float, default=0.125, help='Input augmentation translation')
-    parser.add_argument('-augment_translate_y', type=float, default=0.125, help='Input augmentation translation')
-    parser.add_argument('-augment_noise', type=float, default=0.1, help='Augment image noise')
-
-    parser.add_argument('-resultspath', type=str, default=None)
-    parser.add_argument('-test_dir', type=str, default=None)
-    parser.add_argument('-tensorboard_dir', type=str, default='./tb', 
-        help='to launch the tensorboard server, in the console, enter: tensorboard --logdir ./tb --bind_all')
-
-
-
-    args = parser.parse_args()
-    return args
-
-def save(model, s3, s3def, args):
-    out_buffer = io.BytesIO()
-    model.zero_grad(set_to_none=True)
-    torch.save(model, out_buffer)
-    s3.PutObject(s3def['sets']['model']['bucket'], '{}/{}/{}.pt'.format(s3def['sets']['model']['prefix'],args.model_class,args.model_dest ), out_buffer)
-
 class PlotSearch():
     def __init__(self, network, title = 'Architecture Weights', colormapname = 'jet', lenght = 5, width=7.5, height=20, dpi=1200, fps=2 ):
         self.title = title
@@ -996,6 +916,83 @@ class PlotGradients():
         return img
 
 
+def parse_arguments():
+    import argparse
+    parser = argparse.ArgumentParser(description='Process arguments')
+
+    parser.add_argument('-d', '--debug', action='store_true',help='Wait for debuggee attach')   
+    parser.add_argument('-debug_port', type=int, default=3000, help='Debug port')
+    parser.add_argument('-debug_address', type=str, default='0.0.0.0', help='Debug port')
+    parser.add_argument('-fast', action='store_true', help='Fast run with a few iterations')
+    parser.add_argument('-description', type=str, default="", help='Training description to record with resuts')
+    parser.add_argument('-credentails', type=str, default='creds.json', help='Credentials file.')
+
+    parser.add_argument('-prune', type=str2bool, default=False)
+    parser.add_argument('-train', type=str2bool, default=True)
+    parser.add_argument('-infer', type=str2bool, default=True)
+    parser.add_argument('-search_structure', type=str2bool, default=True)
+    parser.add_argument('-onnx', type=str2bool, default=True)
+    parser.add_argument('-job', action='store_true',help='Run as job')
+
+    parser.add_argument('-resnet_len', type=int, choices=[18, 34, 50, 101, 152, 20, 32, 44, 56, 110], default=56, help='Run description')
+
+    parser.add_argument('-dataset_path', type=str, default='./dataset', help='Local dataset path')
+    parser.add_argument('-model', type=str, default='model')
+
+    parser.add_argument('-learning_rate', type=float, default=1e-1, help='Training learning rate')
+    parser.add_argument('-learning_rate_decay', type=float, default=0.1, help='Rate decay multiple')
+    parser.add_argument('-rate_schedule', type=json.loads, default='[60, 80, 100, 105, 110, 115]', help='Training learning rate')
+    #parser.add_argument('-rate_schedule', type=json.loads, default='[10, 15, 17, 19, 30, 32]', help='Training learning rate')
+    parser.add_argument('-momentum', type=float, default=0.9, help='Learning Momentum')
+    parser.add_argument('-weight_decay', type=float, default=0.0001)
+
+    parser.add_argument('-batch_size', type=int, default=400, help='Training batch size')
+    parser.add_argument('-epochs', type=int, default=20, help='Training epochs')
+    parser.add_argument('-model_type', type=str,  default='Classification')
+    parser.add_argument('-model_class', type=str,  default='CIFAR10')
+    parser.add_argument('-model_src', type=str,  default=None)
+    parser.add_argument('-model_dest', type=str, default="crisp20220209_t50_00")
+    parser.add_argument('-cuda', type=bool, default=True)
+    parser.add_argument('-k_structure', type=float, default=1e1, help='Structure minimization weighting factor')
+    parser.add_argument('-target_structure', type=float, default=0.5, help='Structure minimization weighting factor')
+    parser.add_argument('-batch_norm', type=bool, default=True)
+    parser.add_argument('-dropout_rate', type=float, default=0.0, help='Dropout probability gain')
+    parser.add_argument('-weight_gain', type=float, default=11.0, help='Convolution norm tanh weight gain')
+    parser.add_argument('-sigmoid_scale', type=float, default=5.0, help='Sigmoid scale domain for convolution channels weights')
+    parser.add_argument('-feature_threshold', type=float, default=0.5, help='tanh pruning threshold')
+
+    parser.add_argument('-augment_rotation', type=float, default=0.0, help='Input augmentation rotation degrees')
+    parser.add_argument('-augment_scale_min', type=float, default=1.00, help='Input augmentation scale')
+    parser.add_argument('-augment_scale_max', type=float, default=1.00, help='Input augmentation scale')
+    parser.add_argument('-augment_translate_x', type=float, default=0.125, help='Input augmentation translation')
+    parser.add_argument('-augment_translate_y', type=float, default=0.125, help='Input augmentation translation')
+    parser.add_argument('-augment_noise', type=float, default=0.1, help='Augment image noise')
+
+    parser.add_argument('-resultspath', type=str, default=None)
+    parser.add_argument('-test_dir', type=str, default=None)
+    parser.add_argument('-tensorboard_dir', type=str, default='./tb', 
+        help='to launch the tensorboard server, in the console, enter: tensorboard --logdir ./tb --bind_all')
+
+    args = parser.parse_args()
+    return args
+
+def save(model, s3, s3def, args):
+    out_buffer = io.BytesIO()
+    model.zero_grad(set_to_none=True)
+    torch.save(model, out_buffer)
+    s3.PutObject(s3def['sets']['model']['bucket'], '{}/{}/{}.pt'.format(s3def['sets']['model']['prefix'],args.model_class,args.model_dest ), out_buffer)
+
+    class AddGaussianNoise(object):
+        def __init__(self, mean=0., std=1.):
+            self.std = std
+            self.mean = mean
+            
+        def __call__(self, tensor):
+            return tensor + torch.randn(tensor.size()) * self.std + self.mean
+        
+        def __repr__(self):
+            return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
 # Classifier based on https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 def Test(args):
     import torchvision
@@ -1034,25 +1031,12 @@ def Test(args):
         device = torch.device('cuda')
         pin_memory = True
 
-    class AddGaussianNoise(object):
-        def __init__(self, mean=0., std=1.):
-            self.std = std
-            self.mean = mean
-            
-        def __call__(self, tensor):
-            return tensor + torch.randn(tensor.size()) * self.std + self.mean
-        
-        def __repr__(self):
-            return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
-
     # Load dataset
-    IMAGE_SIZE = 32
     train_transform = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomAffine(degrees=args.augment_rotation, 
             translate=(args.augment_translate_x, args.augment_translate_y), 
             scale=(args.augment_scale_min, args.augment_scale_max), 
             interpolation=transforms.InterpolationMode.BILINEAR),
-        #transforms.RandomCrop(size=IMAGE_SIZE, padding=4),
         transforms.ToTensor(), 
         transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0)), # Imagenet mean and standard deviation
         AddGaussianNoise(0., args.augment_noise)
@@ -1122,7 +1106,6 @@ def Test(args):
     criterion = TotalLoss(args.cuda, k_structure=args.k_structure, target_structure=target_structure, search_structure=args.search_structure)
     optimizer = optim.SGD(classify.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay )
     #optimizer = optim.Adam(classify.parameters(), lr=args.learning_rate)
-    #optimizer = adabound.AdaBound(classify.parameters(), lr=args.learning_rate, final_lr=args.learning_rate)
     plotsearch = PlotSearch(classify)
     plotgrads = PlotGradients(classify)
     scheduler1 = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
@@ -1214,7 +1197,7 @@ def Test(args):
                     cv2.imwrite('gradient_norm.png', plotgrads.plot(classify))
 
                 iSave = 2000
-                if i % iSave == iSave-1:    # print every 20 mini-batches
+                if i % iSave == iSave-1:    # print every iSave mini-batches
                     save(classify, s3, s3def, args)
 
                 if args.fast and i+1 >= test_freq:
@@ -1273,7 +1256,6 @@ def Test(args):
     return 0
 
 if __name__ == '__main__':
-    import argparse
     args = parse_arguments()
 
     if args.debug:
@@ -1306,9 +1288,7 @@ if __name__ == '__main__':
         '''
 
         debugpy.listen(address=(args.debug_address, args.debug_port))
-        # Pause the program until a remote debugger is attached
-
-        debugpy.wait_for_client()
+        debugpy.wait_for_client() # Pause the program until a remote debugger is attached
         print("Debugger attached")
 
     result = Test(args)

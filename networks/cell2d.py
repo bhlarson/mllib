cc99bc45f8bfcd00017b06a88107a9bc89bbc7ff
Brad Larson <bhlarson@gmail.com>
2022-03-03 22:07:58 -0800

    Mach GaussianBasis to notes & normal distribution computaiton

--- networks/cell2d.py
+++ networks/cell2d.py
@@ -35,7 +35,7 @@ from networks.totalloss import TotalLoss
 # Process: Con2d, optional batch norm, optional ReLu
 
 def GaussianBasis(i, zero=0.0, sigma=0.33):
-    return torch.exp(-1*(sigma*(i-zero))*(sigma*(i-zero))) # torch.square not supported by torch.onnx
+    return torch.exp(-1*torch.square((i-zero)/(2*sigma))) # torch.square not supported by torch.onnx
 
 def NormGausBasis(len, i, depth, r=1.0):
         den = 0.0

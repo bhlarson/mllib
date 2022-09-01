Working but incorrect flops computation

networks/cell2d.py
@@ -37,15 +37,21 @@ from networks.totalloss import TotalLoss
 # Process: Con2d, optional batch norm, optional ReLu
 
 def relu_flops_counter_hook(module, shape, relaxation):
-    channel_weight = relaxation.weights()
-    num_channels = len(channel_weight)
-    active_elements_count = torch.prod(torch.Tensor(list(shape))) * torch.sum(channel_weight)/num_channels
+    if relaxation is not None:
+        channel_weight = relaxation.weights()
+        num_channels = len(channel_weight)
+        active_elements_count = torch.prod(torch.Tensor(list(shape))) * torch.sum(channel_weight)/num_channels
+    else:
+        active_elements_count = torch.prod(torch.Tensor(list(shape)))
     return active_elements_count
 
-def bn_flops_counter_hook(module, shape, sigmoid):
-    channel_weight = torch.sum(sigmoid)
-    num_channels = len(sigmoid)
-    batch_flops = torch.prod(torch.Tensor(list(shape))) * channel_weight/num_channels
+def bn_flops_counter_hook(module, shape, relaxation):
+    if relaxation is not None:
+        channel_weight = torch.sum(relaxation)
+        num_channels = len(relaxation)
+        batch_flops = torch.prod(torch.Tensor(list(shape))) * channel_weight/num_channels
+    else:
+        batch_flops = torch.prod(torch.Tensor(list(shape)))
     if module.affine:
         batch_flops *= 2
     return batch_flops
@@ -64,8 +70,11 @@ def conv_flops_counter_hook(conv_module, in_relaxation, output_shape, out_relaxa
         in_channel_weight = torch.sum(torch.cat(input_relaxation, 0))
     else:
         in_channel_weight = conv_module.weight.shape[0]
-    #out_channels = conv_module.out_channels
-    out_channel_weight = torch.sum(out_relaxation.weights())
+
+    if out_relaxation is not None:
+        out_channel_weight = torch.sum(out_relaxation.weights())
+    else:
+        out_channel_weight = conv_module.out_channels
     groups = conv_module.groups
 
     filters_per_channel = out_channel_weight / groups
@@ -308,14 +317,14 @@ class ConvBR(nn.Module):
         cell_weights = model_weights(self)
 
         if self.search_flops:
-            convbr_flops = 0
+            convbr_flops = torch.zeros(1, device=self.device)
             if self.activation:
                 convbr_flops += relu_flops_counter_hook(self.activation, self.output_shape, self.relaxation)
             if self.batch_norm:
                 convbr_flops += bn_flops_counter_hook(self.batchnorm2d, self.output_shape, self.relaxation)
             convbr_flops += conv_flops_counter_hook(self.conv, self.prev_relaxation, self.output_shape, self.relaxation)
 
-            architecture_weights = convbr_flops/ self.out_channels
+            architecture_weights = torch.tensor([convbr_flops/ self.out_channels])
 
         else:
 
@@ -560,13 +569,13 @@ class Cell(nn.Module):
                                      weight_gain=weight_gain, sigmoid_scale=sigmoid_scale, k_prune_sigma=k_prune_sigma, search_flops=search_flops)
 
     def ArchitectureWeights(self):
-        architecture_weights = []
         layer_weights = []
         conv_weights = []
         weight_basises = []
         norm_conv_weight = []
         search_structure = []
         unallocated_weights  = torch.zeros((1), device=self.device)
+        architecture_weights = torch.zeros((1), device=self.device)
         if self.cnn is not None:
             for i, l in enumerate(self.cnn): 
                 layer_weight, cnn_weight, conv_weight, weight_basis = l.ArchitectureWeights()
@@ -574,14 +583,12 @@ class Cell(nn.Module):
                 weight_basises.append(weight_basis)
 
                 if self.convolutions[i]['search_structure']:
-                    architecture_weights.append(layer_weight)
+                    architecture_weights += layer_weight[0]
                     norm_conv_weight.append(layer_weight/cnn_weight)
                 else:
                     unallocated_weights += cnn_weight
 
-            if len(architecture_weights) > 0:
-                architecture_weights = torch.cat(architecture_weights,0)
-                architecture_weights = architecture_weights.sum_to_size((1))
+            if architecture_weights[0] > 0:
                 num_conv_weights = len(norm_conv_weight)
                 if num_conv_weights > 0:
                     # Allocate remaining weights if pruning full channel
@@ -1243,9 +1250,9 @@ class PlotGradients():
                     layer_norms = []
                     if convbr.conv.weight.grad is not None:
                         if convbr.conv_transpose:
-                            grads = torch.linalg.norm(convbr.conv.weight.grad , dim=(0,2,3))/np.sqrt(np.product(convbr.conv.weight.grad.shape[1:]))
+                            grads = convbr.conv.weight.grad.permute(1, 0, 2, 3).flatten(1).norm(dim=1)/np.sqrt(np.product(convbr.conv.weight.grad.shape[1:]))
                         else:
-                            grads = torch.linalg.norm(convbr.conv.weight.grad , dim=(1,2,3))/np.sqrt(np.product(convbr.conv.weight.grad.shape[0:]))
+                            grads = convbr.conv.weight.grad.flatten(1).norm(dim=1)/np.sqrt(np.product(convbr.conv.weight.grad.shape[0:]))
                         numSums = 1                    
 
                         if self.pruning:

networks/network2d.py
@@ -449,7 +449,7 @@ def MakeNetwork2d(class_dictionary, args):
     if args.cuda:
         device = torch.device("cuda")
 
-    return Network2d(class_dictionary['classes'], source_channels=class_dictionary['input_channels'],
+    network = Network2d(class_dictionary['classes'], source_channels=class_dictionary['input_channels'],
             device=device, 
             unet_depth=args.unet_depth,
             max_cell_steps=args.max_cell_steps, 
@@ -462,7 +462,13 @@ def MakeNetwork2d(class_dictionary, args):
             convMaskThreshold = args.convMaskThreshold,
             dropout_rate = args.dropout_rate,
             sigmoid_scale = args.sigmoid_scale,
-            k_prune_sigma = args.k_prune_sigma)
+            k_prune_sigma = args.k_prune_sigma,
+            search_flops = args.search_flops)
+	
+	#specify device for model
+    network.to(device)
+
+    return network
 
 def load(s3, s3def, args, class_dictionary, results):
     segment = None
@@ -470,6 +476,10 @@ def load(s3, s3def, args, class_dictionary, results):
     if 'initial_parameters' not in results or args.model_src is None or args.model_src == '':
         segment = MakeNetwork2d(class_dictionary, args)
         results['initial_parameters'] = count_parameters(segment)
+
+        macs, params = get_model_complexity_info(segment, (class_dictionary['input_channels'], args.height, args.width), as_strings=False,
+                                            print_per_layer_stat=False, verbose=False)
+        results['initial_flops'] = 2*macs
 
     if(args.model_src and args.model_src != ''):
         modelObj = s3.GetObject(s3def['sets']['model']['bucket'], '{}/{}/{}.pt'.format(s3def['sets']['model']['prefix'],args.model_class,args.model_src ))
@@ -480,7 +490,7 @@ def load(s3, s3def, args, class_dictionary, results):
             print('Failed to load model_src {}/{}/{}/{}.pt  Exiting'.format(s3def['sets']['model']['bucket'],s3def['sets']['model']['prefix'],args.model_class,args.model_src))
             return segment
 
-    print('load initial_parameters = {}'.format(results['initial_parameters']))
+    print('load initial_parameters = {} initial_flops = {}'.format(results['initial_parameters'], results['initial_flops']))
 
 
     return segment, results
@@ -617,6 +627,10 @@ def Train(args, s3, s3def, class_dictionary, segment, device, results):
     #         record_shapes=True, profile_memory=True, with_stack=True
     # ) as prof:
     if True:
+        if args.search_flops:
+            total_weights= results['initial_flops'] 
+        else:
+            total_weights= results['initial_parameters'] 
         loss_fcn = TotalLoss(args.cuda,
                              k_accuracy=args.k_accuracy,
                              k_structure=args.k_structure, 
@@ -627,7 +641,7 @@ def Train(args, s3, s3def, class_dictionary, segment, device, results):
                              k_prune_exp=args.k_prune_exp,
                              sigmoid_scale=args.sigmoid_scale,
                              ejector=args.ejector,
-                             total_weights= results['initial_parameters'],
+                             total_weights= total_weights,
                              )
         #optimizer = optim.SGD(segment.parameters(), lr=args.learning_rate, momentum=0.9)
         optimizer = optim.Adam(segment.parameters(), lr= args.learning_rate)
@@ -935,6 +949,10 @@ def Prune(args, s3, s3def, class_dictionary, segment, device, results):
     if not 'initial_parameters' in results:
         full_model = MakeNetwork2d(class_dictionary, args)
         results['initial_parameters'] = count_parameters(full_model)
+        macs, params = get_model_complexity_info(full_model, (class_dictionary['input_channels'], args.height, args.width), as_strings=False,
+                                            print_per_layer_stat=False, verbose=False)
+        results['initial_flops'] = 2*macs
+
     initial_parameters = results['initial_parameters']
     segment.ApplyStructure()
     reduced_parameters = count_parameters(segment)
@@ -1000,13 +1018,16 @@ def main(args):
                 results['batches'] = prevresults['batches']
             if 'initial_parameters' in prevresults:
                 results['initial_parameters'] = prevresults['initial_parameters']
+                results['initial_flops'] = prevresults['initial_flops']
 
     segment, results = load(s3, s3def, args, class_dictionary, results)
 
     if not 'initial_parameters' in results:
         full_model = MakeNetwork2d(class_dictionary, args)
         results['initial_parameters'] = count_parameters(full_model)
-        
+        macs, params = get_model_complexity_info(full_model, (class_dictionary['input_channels'], args.height, args.width), as_strings=False,
+                                            print_per_layer_stat=False, verbose=False)
+        results['initial_flops'] = 2*macs
 
     # Prune with loaded parameters than apply current search_structure setting
     segment.ApplyParameters(weight_gain=args.weight_gain, 
@@ -1022,9 +1043,9 @@ def main(args):
     # model_copy = copy.deepcopy(segment)
     macs, params = get_model_complexity_info(segment, (class_dictionary['input_channels'], args.height, args.width), as_strings=False,
                                         print_per_layer_stat=False, verbose=False)
-    print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
-    print('{:<30}  {:<8}'.format('Number of parameters: ', params))
     results['initial_flops'] = 2*macs
+    print('{:<30}  {:<8}'.format('FLOPS: ', results['initial_flops'] ))
+    print('{:<30}  {:<8}'.format('Number of parameters: ', params))
 
     # Train
     if args.train:
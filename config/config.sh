1871046e327f7503c716e1e12dc39c7feae0e75a
Brad Larson <bhlarson@gmail.com>
2023-02-14 08:35:55 -0800

    Update versions

--- config/config.sh
+++ config/config.sh
@@ -1,3 +1,3 @@
-export VERSION=0.0.446
-export crisptrain=localhost:32000/crisptrain:0.0.446
-export crispdev=localhost:32000/crispdev:0.0.446
+export VERSION=0.0.447
+export crisptrain=localhost:32000/crisptrain:0.0.447
+export crispdev=localhost:32000/crispdev:0.0.447

5f674b3820e1cf4865a52dac2f5eee191273ab6d
Brad Larson <bhlarson@gmail.com>
2023-03-10 07:08:22 -0800

    Tag version 0.0.472

--- config/config.sh
+++ config/config.sh
@@ -1,3 +1,3 @@
-export VERSION=0.0.471
-export crisptrain=localhost:32000/crisptrain:0.0.471
-export crispdev=localhost:32000/crispdev:0.0.471
+export VERSION=0.0.472
+export crisptrain=localhost:32000/crisptrain:0.0.472
+export crispdev=localhost:32000/crispdev:0.0.472
